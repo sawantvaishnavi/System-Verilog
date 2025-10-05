a2a435c00ed941b19697e05167af3f4f97470275
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-10-05 06:22:17 +0530

    Update Q4.Function to count number of 1’s in 4-bit input.sv

--- Function & Task/Q4.Function to count number of 1’s in 4-bit input.sv
+++ Function & Task/Q4.Function to count number of 1’s in 4-bit input.sv
@@ -1,62 +1,85 @@
-//1.
-module tb();
+//A. number of 1 in 4 bit number
+module tb;
   
-  function bit [8:0] add(input bit [7:0] x, input bit [7:0] y);
-   return x + y;
+  function int count (input [3:0] num);
+    int cnt = 0;
+    for(int i=0; i<4; i++) begin
+     if (num[i])
+       cnt = cnt +1;
+      else
+        cnt = cnt;
+    end  
+    return cnt; 
   endfunction
+ 
   
+  initial begin 
+    int total_count;
+    total_count = count(4'b1011);
+    $display("Total count of 1 is: %0d",total_count);
+  end
   
-  bit [8:0] sum;
+endmodule
+
+
+//b. find num of 1 in given number width is variable
+//$size() only works on arrays, not on scalar variables like int.
+// use $bits(num) to get bit width.
+
+module tb;
   
+  function int count(int num);
+   int cnt = 0;
+    for(int i =0; i<$bits(num); i++) begin
+      if(num[i])
+        cnt = cnt + 1 ;
+    end
+    return cnt;     
+  
+    
+  endfunction
+ 
   initial begin
-    sum = add(8,7);
-    $display("sum is: %0d", sum);
+    int total_count_binary;
+//     int total_count_decimal;
+//     int total_count_hexadecimal;
+//     int total_count_octal;
+   
+    
+    total_count_binary = count('b1101100111);
+    $display("Total Count of 1's is: %0d", total_count_binary);
+    
+//     total_count_decimal = count('d15);
+//     $display("Total Count of 1's is: %0d", total_count_decimal);
+        
+//     total_count_hexadecimal = count('hAAF);
+//     $display("Total Count of 1's is: %0d", total_count_hexadecimal);
+    
+    
+//     total_count_octal = count('o712);
+//     $display("Total Count of 1's is: %0d", total_count_octal);
   end
   
 endmodule
 
 
 
-//2.
+
+//C. Counting number of 1 using recursion
+
 module tb();
   
-  function bit [8:0] add(input bit [7:0] x, input bit [7:0] y);
-    add = x +y ;         /// here add here is not a variable you declared — it’s actually the name of the function itself.
-//In SystemVerilog, the function name acts as an implicit variable of the same data type as the function’s return type.
-    
-    $display("sum is: %0d", add);
+  function int count(input int num);
+    if (num == 0)
+      return 0;
+    else
+      return (num & 1) + count(num >> 1);
   endfunction
   
-  
-  bit [8:0] sum;
-  
   initial begin
-    sum = add(5,7);
+    int answer;
+    answer = count('b011001100111);         ///always when u r assigning binary value give 'b or else it will consider as decimal value by default
+    $display("Total Count of 1's is: %0d", answer);
     
   end
-  
 endmodule
-
-
-
-
-//3.
-module tb();
-  
-  function bit [8:0] add(input bit [7:0] x, input bit [7:0] y);
-   return x + y;
-  endfunction
-  
-  
-  bit [8:0] sum;
-  
-  initial begin
-    bit [7:0] x = 8;
-    bit [7:0] y = 7;
-    sum = add(x,y);
-    
-    $display("sum is: %0d", sum);
-  end
-  
-endmodule
-
