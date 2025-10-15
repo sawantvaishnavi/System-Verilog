451f7614cae3a8c4778f9e43ad2ce87fdc09fd09
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-10-15 17:21:46 +0530

    Update Q1.Constrain three variables a, b, c such that their sum equals 100.sv

--- Randomization/Q1.Constrain three variables a, b, c such that their sum equals 100.sv
+++ Randomization/Q1.Constrain three variables a, b, c such that their sum equals 100.sv
@@ -1,1 +1,36 @@
+//Constrain three variables a, b, c such that their sum equals 100.
+
+class generator;
+  rand bit [7:0] a;
+  rand bit [7:0] b;
+  rand bit [7:0] c;
+ 
+  
+  constraint conditions1 {
+    
+    a+b+c ==100;
+    a inside {[0:100]};
+    b inside {[0:100]};
+    c inside {[0:100]};
+}
+  
+
+  
+endclass
+
+module tb;
+  
+  generator g;
+  initial begin
+    g = new();
+    for (int i=0; i<20; i++) begin
+      g.randomize();
+      $display("At time: %0t, a=%0d, b=%0d, c=%0d, sum:%0d", $time, g.a, g.b, g.c, g.a+g.b+g.c);
+      
+      #10;
+    end
+  end
+endmodule
+
+
 
