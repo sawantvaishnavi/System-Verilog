3a7371bc76bc573a05ab77d606066b99b3eb37d7
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-10-19 13:07:56 +0530

    Update Q1.Write three tasks that print messages with different delays and run them together using fork-join, fork-join_any, fork-join_none.sv

--- IPC/Fork & Join Family/Q1.Write three tasks that print messages with different delays and run them together using fork-join, fork-join_any, fork-join_none.sv
+++ IPC/Fork & Join Family/Q1.Write three tasks that print messages with different delays and run them together using fork-join, fork-join_any, fork-join_none.sv
@@ -1,3 +1,36 @@
+//1.Without forever loop
+module tb;
+  
+  task T1;
+      $display("Task 1 Started %0t", $time);
+      #20;
+      $display("Task 1 Completed %0t", $time);        
+  endtask
+  
+  task T2;
+      $display("Task 2 Started %0t", $time);
+      #30;
+      $display("Task 2 Completed %0t", $time);
+  endtask
+  
+  task T3;
+    $display("Reached next to Join at %0t",$time);
+  endtask
+  
+  initial begin  
+    fork
+      T1 (); 
+      T2 ();
+    join_none
+
+      T3 ;
+    end
+endmodule
+
+
+
+
+//2. Using forever loop 
 module tb;
   
   task T1;
