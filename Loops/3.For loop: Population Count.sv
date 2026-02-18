e99714ea136f3a4f366d3721b7f44d775831057c
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-02-18 14:33:35 +0530

    Update 3.For loop: Population Count.sv

--- Loops/3.For loop: Population Count.sv
+++ Loops/3.For loop: Population Count.sv
@@ -14,7 +14,7 @@ module population_count (
     end
 endmodule
 
-/// Tb ///
+/// Tb 1: using urandom///
 module tb();
   logic [15:0] din;
   logic [4:0] count;
@@ -28,4 +28,49 @@ module tb();
       $display("din=%0b, count=%0d", din, count);
     end
   end
+endmodule
+
+/// Tb 2: using assertion  (using $countones(din) function )  //
+module tb();
+  logic [15:0] din;
+  logic [4:0] count;
+  
+  population_count dut (din, count);
+
+  initial begin
+    for(int i=0; i<5; i++) begin
+      din = $urandom;
+      #10;
+      $display("din=%0b, count=%0d", din, count);
+    
+    
+    if(count == $countones(din)) 
+      $display(" Counting is Correct!! ");
+    else
+      $error("mismatched");
+    end
+  end
+endmodule
+
+
+//Tb 3: using assert property //
+/* Que. Why do we use ##0 in combinational assertions?
+To avoid race between signal update and assertion sampling due to delta cycle scheduling. */
+module tb();
+  logic [15:0] din;
+  logic [4:0] count;
+  
+  population_count dut (din, count);
+
+  initial begin
+    for(int i=0; i<5; i++) begin
+      din = $urandom;
+      #10;
+      $display("din=%0b, count=%0d", din, count);
+    end
+  end
+  
+  assert property (@(din) ##0 count == $countones(din))
+    $info("Counting is Correct!!");
+    else $error("mismatched");
 endmodule
