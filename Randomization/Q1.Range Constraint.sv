14cb76101e521418ffdfecbd2b00778a9f12f9df
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-03-02 17:35:17 +0530

    Update Q1.Range Constraint.sv

--- Randomization/Q1.Range Constraint.sv
+++ Randomization/Q1.Range Constraint.sv
@@ -5,28 +5,22 @@ class generator;
   rand bit [7:0] b;
   rand bit [7:0] c;
  
-  
   constraint conditions1 {
-    
-    a+b+c ==100;
+    a+b+c == 100;
     a inside {[0:100]};
     b inside {[0:100]};
     c inside {[0:100]};
-}
-  
-
-  
+} 
 endclass
 
-module tb;
-  
+
+module tb; 
   generator g;
   initial begin
-    g = new();
     for (int i=0; i<20; i++) begin
+      g = new();     /// Each iteration new object is created
       g.randomize();
       $display("At time: %0t, a=%0d, b=%0d, c=%0d, sum:%0d", $time, g.a, g.b, g.c, g.a+g.b+g.c);
-      
       #10;
     end
   end
